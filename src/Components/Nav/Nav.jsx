0a2765021f363fe99dc8fc5ea16bcf867fd1bed5
Franco Sanchez <pacosanchez0196@gmail.com>
2021-11-23 00:05:05 -0300

    Commit clase 3

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const Nav = () => {
+    return (
+        <ul className ="navContainer">
+            <li className="list">Home</li>
+            <li className="list">Categoria A</li>
+            <li className="list">Categoria B</li>
+        </ul>
+    )
+}
+
+export default Nav
